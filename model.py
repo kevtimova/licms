33ad5e1d17e07f7898b846c33204ae1eadf5d623
Katrina Evtimova <kve216@nyu.edu>
2018-11-29 00:02:05 -0500

    sample architecture for experts and discriminator

---
+++ model.py
@@ -0,0 +1,49 @@
+import torch.nn as nn
+
+class Expert(nn.Module):
+
+    def __init__(self, args):
+        super(Expert, self).__init__()
+        self.args = args
+
+        # Architecture
+        def block(in_feat, out_feat, normalize=True):
+            layers = [nn.Linear(in_feat, out_feat)]
+            if normalize:
+                layers.append(nn.BatchNorm1d(out_feat, 0.8))
+            layers.append(nn.LeakyReLU(0.2, inplace=True))
+            return layers
+
+        self.model = nn.Sequential(
+            *block(self.args.input_size, 128, normalize=False),
+            *block(128, 256),
+            *block(256, 512),
+            *block(512, 1024),
+            nn.Linear(1024, int(self.args.input_size)),
+            nn.Tanh()
+        )
+
+    def forward(self, input):
+        output = self.model(input)
+        return output
+
+
+class Discriminator(nn.Module):
+
+    def __init__(self, args):
+        super(Discriminator, self).__init__()
+        self.args = args
+
+        # Architecture
+        self.model = nn.Sequential(
+            nn.Linear(self.args.input_size, 512),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(512, 256),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(256, 1),
+            nn.Sigmoid()
+        )
+
+    def forward(self, input):
+        validity = self.model(input)
+        return validity
