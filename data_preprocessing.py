cf8452d6ab297831d8402d290d0d6157b49cd879
Katrina Evtimova <kve216@nyu.edu>
2018-11-30 10:58:46 -0500

    joining LABEVENTs with Sepsis and ADMISSIONS

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -2,9 +2,9 @@ import pandas as pd
 import argparse
 import os
 
-def read_admissions(file_loc, columns=None):
-    admissions = pd.read_csv(file_loc, delimiter=',', usecols=columns)
-    import ipdb; ipdb.set_trace()
+def read_data(file_loc, columns=None):
+    data = pd.read_csv(file_loc, delimiter=',', usecols=columns)
+    return data
 
 def main():
     parser = argparse.ArgumentParser(description='MIMIC III')
@@ -19,9 +19,92 @@ def main():
     
     PATIENTS.csv
     SUBJECT_ID, GENDER, DOB, DOD, DOD_HOSP, DOD_SSN, EXPIRE_FLAG
+    
+    LABEVENTS.csv
+    
+    D_LABITEMS.csv
+    
+    
+    DEMOGRAPHIC/STATIC
+    Shock Index
+    Elixhauser
+    SIRS
+    Gender
+    Re-admission
+    GCS - Glasgow Coma Scale
+    SOFA - Se- quential Organ Failure Assessment
+    Age
+    
+    LAB VALUES
+    Albumin
+    Arterial pH
+    Calcium
+    Glucose
+    Haemoglobin
+    Magnesium
+    PTT - Partial Thromboplastin Time
+    Potassium
+    SGPT - Serum Glutamic-Pyruvic Transaminase
+    Arterial Blood Gas
+    BUN - Blood Urea Nitrogen
+    Chloride
+    Bicarbonate
+    INR - International Normalized Ratio
+    Sodium
+    Arte- rial Lactate
+    CO2
+    Creatinine
+    Ionised Calcium
+    PT - Prothrombin Time
+    Platelets Count
+    SGOT - Serum Glutamic-Oxaloacetic Transaminase
+    Total bilirubin
+    White Blood Cell Count
+    
+    VITAL SIGNS
+    Diastolic Blood Pressure
+    Systolic Blood Pressure
+    Mean Blood Pressure
+    PaCO2
+    PaO2
+    FiO2
+    PaO/FiO2 ratio
+    Respiratory Rate
+    Temperature (Celsius)
+    Weight (kg)
+    Heart Rate
+    SpO2
+    
+    INTAKE AND OUTPUT EVENTS
+    Fluid Output - 4 hourly period
+    Total Fluid Output
+    Mechanical Ventilation    
     """
+
+    # Admissions
+    cols = ['subject_id', 'hadm_id', 'admittime', 'dischtime',
+            'has_chartevents_data']
     admissions_loc = os.path.join(args.datadir, "ADMISSIONS.csv")
-    read_admissions(admissions_loc)
+    admissions = read_data(admissions_loc)
+    admissions.columns = admissions.columns.str.lower()
+
+
+    """
+    Sepsis Data
+    """
+    sepsis_loc = os.path.join(args.datadir, "sepsis3-data/sepsis3-df.csv")
+    sepsis = read_data(sepsis_loc)
+
+    """
+    LABEVENTS.csv
+    """
+    labevents = os.path.join(args.datadir, "LABEVENTS.csv")
+    labevents.columns = labevents.columns.str.lower()
+
+    # Join Sepsis and ADMISSIONS
+    joined_data = sepsis.merge(admissions, on=['hadm_id'])
+    joined_data = joined_data.merge(labevents, on=['subject_id', 'hadm_id'])
+    joined_data.to_csv(os.path.join(args.datadir, "joined_data.csv"))
 
 
 if __name__ == '__main__':
