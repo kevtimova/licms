eb3ea5320ce81fdfa9af8544491a10b2dc1b2913
Katrina Evtimova <kve216@nyu.edu>
2018-12-01 01:27:17 -0500

    training regime

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from torch.autograd import Variable
 
 import argparse
 import json
@@ -7,6 +8,65 @@ import numpy as np
 import time
 
 from model import Expert, Discriminator
+
+def initialize_expert(epochs, expert, optimizer, loss, data_train, args):
+    expert.train()
+    losses = []
+
+    for epoch in range(epochs):
+        for batch in data_train:
+            x_real, x_pret = batch
+            x_real = Variable(x_real).to(args.device)
+            x_hat = expert(x_real)
+            loss_rec = loss(x_real, x_hat)
+            losses.append(loss_rec)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+        # TODO: make sure loss is computed correctly
+        mean_loss = np.array([loss.cpu().data.item() for loss in losses]).mean()
+        print("epoch [{}] {}".format(epoch+1, mean_loss))
+
+def train_system(epoch, experts, discriminator, optimizers_E, optimizer_D, loss, data_train, args):
+    canonical_label = 1
+    generated_label = 0
+    losses = []
+    for batch in data_train:
+        batch_size = None # TODO
+        x_canon, x_pret = batch
+        x_canon = Variable(x_canon).to(args.device)
+        x_pret = Variable(x_pret).to(args.device)
+
+        # Train Discriminator on canonical distribution
+        scores = discriminator(x_canon)
+        labels = torch.full((batch_size,), canonical_label, device=args.device)
+        loss_D_cannon = loss(scores, labels)
+        optimizer_D.zero_grad()
+        loss_D.backward()
+
+        # Train Discriminator on experts output
+        outputs = []
+        labels.fill_(generated_label)
+        loss_D_generated = 0 # TODO make compatible with output
+        expert_scores = torch.FloatTensor().cuda() if args.cuda else torch.FloatTensor()
+        for i, expert in enumerate(experts):
+            output = expert(x_pret)
+            scores = discriminator(output.detach())
+            torch.cat(expert_scores, scores) # TODO check dim
+            loss_D_generated += criterion(output, scores)
+        loss_D_generated = loss_D_generated / args.num_experts
+        loss_D_generated.backward()
+        optimizer_D.step()
+
+        expert_winners = None # TODO best expert for each data point
+        # TODO update each expert
+        for i, expert in enumerate(experts):
+            labels.fill_(canonical_label)
+            optimizers_E[i].zero_grad()
+
+
+
 
 if __name__ == '__main__':
     # Arguments
@@ -21,6 +81,8 @@ if __name__ == '__main__':
                         help='input batch size for training (default: 128)')
     parser.add_argument('--epochs', type=int, default=100, metavar='N',
                         help='number of epochs to train (default: 10)')
+    parser.add_argument('--epochs_init', type=int, default=100, metavar='N',
+                        help='number of epochs to initially train experts (default: 10)')
     parser.add_argument('--no_cuda', action='store_true', default=False,
                         help='enables CUDA training')
     parser.add_argument('--seed', type=int, default=11, metavar='S',
@@ -57,23 +119,30 @@ if __name__ == '__main__':
     print('\nExperiment: {}\n'.format(args.name))
 
     # Load Data
+    data_train = None # TODO: load data
 
     # Model
-    expert = Expert(args)
+    experts = [Expert(args) for i in range(args.num_experts)]
     discriminator = Discriminator(args)
 
     # Losses
-    loss = torch.nn.BCELoss()
+    loss_initial = torch.nn.MSELoss() # TODO: mean or sum reduction
+    criterion = torch.nn.BCELoss() # TODO: pick the right loss
 
     # Optimizers
-    optimizer_E = torch.optim.Adam(expert.parameters(), lr=args.learning_rate, weight_decay=args.weight_decay)
+    optimizers_E = []
+    for i in range(args.num_experts)
+        optimizer_E = torch.optim.Adam(experts[i].parameters(), lr=args.learning_rate, weight_decay=args.weight_decay)
+        optimizers_E.append(optimizer_E)
     optimizer_D = torch.optim.Adam(discriminator.parameters(), lr=args.learning_rate, weight_decay=args.weight_decay)
 
     # Initialize Experts as approximately Identity
+    for i, expert in enumerate(experts):
+        initialize_expert(args.epochs_init, expert, optimizers_E[i], loss_init, data_train, args)
 
     # Training
     for epoch in range(args.epochs):
-        # Train Discriminator
 
-        # Train Experts
+        train_system()
+
 
