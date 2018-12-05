70b1c2e9197dfd5532eefd7bce674e517db4facc
Katrina Evtimova <kve216@nyu.edu>
2018-12-05 17:33:31 -0500

    sepsis dataset class

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,6 +1,31 @@
 import pandas as pd
 import argparse
 import os
+import numpy as np
+
+from torch.utils.data.dataset import Dataset
+
+class SepsisDataset(Dataset):
+    def __init__(self, data_path, transforms=None):
+        self.before_path = os.path.join(data_path, "sepsis_before.csv")
+        self.after_path = os.path.join(data_path, "sepsis_after.csv")
+        self.before_data = pd.read_csv(self.before_path)
+        self.after_data = pd.read_csv(self.after_path)
+        self.transforms = transforms
+
+    def __getitem__(self, index):
+        x_before = np.array(self.before_data.iloc[index])
+        x_after = np.array(self.after_data.iloc[index])
+        if self.transforms is not None:
+            x_before = self.transforms(x_before)
+            x_after = self.transforms(x_after)
+        return (x_before, x_after)
+
+    def __len__(self):
+        len_before = len(self.before_data)
+        len_after = len(self.after_data)
+        assert(len_before == len_after)
+        return len_before
 
 def read_data(file_loc, columns=None, chunksize=None):
     data = pd.read_csv(file_loc, delimiter=',', usecols=columns, chunksize=chunksize)
@@ -219,14 +244,20 @@ def main():
     sepsis_aggregated_before_int[vars_to_normalize] = (sepsis_aggregated_before_int[vars_to_normalize] -
                                                        sepsis_aggregated_before_int[vars_to_normalize].mean()) / \
                                                        sepsis_aggregated_before_int[vars_to_normalize].std()
+    len_after = len(sepsis_aggregated_after_int)
+    len_before = len(sepsis_aggregated_before_int)
+    n_samples = len_after - len_before
+    samples = sepsis_aggregated_before_int.sample(n=n_samples, random_state=11, replace=True)
+    sepsis_aggregated_before_int = sepsis_aggregated_before_int.append(samples)
     data_before_loc = os.path.join(args.datadir, "sepsis_before.csv")
-    sepsis_aggregated_before_int.to_csv(data_before_loc, index=False)
+    sepsis_aggregated_before_int.to_csv(data_before_loc, index=False, header=False)
+
     sepsis_aggregated_after_int = sepsis_aggregated_after_int[dummy_vars + vars_to_normalize]
     sepsis_aggregated_after_int[vars_to_normalize] = (sepsis_aggregated_after_int[vars_to_normalize] -
                                                       sepsis_aggregated_after_int[vars_to_normalize].mean()) / \
                                                       sepsis_aggregated_after_int[vars_to_normalize].std()
     data_after_loc = os.path.join(args.datadir, "sepsis_after.csv")
-    sepsis_aggregated_after_int.to_csv(data_after_loc, index=False)
+    sepsis_aggregated_after_int.to_csv(data_after_loc, index=False, header=False)
 
     if __name__ == '__main__':
-    main()
+        main()
