e11cb1c1a9d1048383c0cd6501487df01e0ae8c5
Katrina Evtimova <kve216@nyu.edu>
2018-12-05 19:15:19 -0500

    add im_size

--- patient_data.py
+++ patient_data.py
@@ -1,23 +1,24 @@
 import pandas as pd
 import os
 import numpy as np
+import torch
 
 from torch.utils.data.dataset import Dataset
 
 class PatientsDataset(Dataset):
-    def __init__(self, data_path, transforms=None):
-        self.before_path = os.path.join(data_path, "sepsis_before.csv")
-        self.after_path = os.path.join(data_path, "sepsis_after.csv")
-        self.before_data = pd.read_csv(self.before_path)
-        self.after_data = pd.read_csv(self.after_path)
-        self.transforms = transforms
+    def __init__(self, args):
+        self.before_path = os.path.join(args.datadir, "sepsis_before.csv")
+        self.after_path = os.path.join(args.datadir, "sepsis_after.csv")
+        self.before_data = pd.read_csv(self.before_path, header=None)
+        self.after_data = pd.read_csv(self.after_path, header=None)
+        self.args = args
+        self.im_size = int(self.before_data.size/len(self.before_data))
 
     def __getitem__(self, index):
         x_before = np.array(self.before_data.iloc[index])
         x_after = np.array(self.after_data.iloc[index])
-        if self.transforms is not None:
-            x_before = self.transforms(x_before)
-            x_after = self.transforms(x_after)
+        x_before = torch.FloatTensor(x_before).to(self.args.device)
+        x_after = torch.FloatTensor(x_after).to(self.args.device)
         return (x_before, x_after)
 
     def __len__(self):
