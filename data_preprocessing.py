b97dc395948261f189f2b7698c0622ff0c50cf1f
Katrina Evtimova <kve216@nyu.edu>
2018-12-08 15:48:46 -0500

    looking for patients with SOFA

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -166,11 +166,19 @@ def main():
         print("Merging chunk {} ...".format(i+1))
         chunk.columns = chunk.columns.str.lower()
         filtered_chunk = chunk.merge(d_items_final, on=['itemid'], how='inner')
-        merged_chunk = filtered_chunk[['subject_id', 'hadm_id']].unique()
-        import ipdb; ipdb.set_trace()
-        sofa_patients = chartevents.append(merged_chunk)
-    chartevents_output = os.path.join(args.datadir, "sepsis_chartevents.csv")
-    chartevents.to_csv(chartevents_output, index=False)
+        # merged_chunk = filtered_chunk[['subject_id', 'hadm_id']].drop_duplicates()
+        filtered_chunk = filtered_chunk[filtered_chunk['itemid'] == 227428][['subject_id', 'hadm_id']].drop_duplicates()
+        sofa_patients = sofa_patients.append(filtered_chunk)
+    sofa_patients_output = os.path.join(args.datadir, "sofa_patients.csv")
+    sofa_patients.to_csv(sofa_patients_output, index=False)
+
+    # """
+    # LABITEMS and LABEVENTS
+    # """
+    # labitems_loc = os.path.join(args.datadir, "D_LABITEMS.csv")
+    # labitems = read_data(labitems_loc)
+    #
+    # labevents_loc = os.path.join(args.datadir, "D_LABITEMS.csv")
 
     # """
     # LABEVENTS.csv
