14d97917551477c88c4179e3b3401506ddc4f9f3
Katrina Evtimova <kve216@nyu.edu>
2018-12-16 10:00:00 -0500

    method for initializing state dict with pre-trained model

--- utils.py
+++ utils.py
@@ -9,3 +9,11 @@ class LossReconstruction:
         sq_diff = torch.pow(target - pred, 2)
         sum_sq_diff = sq_diff.view(sq_diff.size(0), -1).sum(1)
         return sum_sq_diff.mean()
+
+
+def init_weights(expert, path):
+    pre_trained_dict = torch.load(path, map_location=lambda storage, loc: storage)
+    for layer in pre_trained_dict.keys():
+        expert.state_dict()[layer].copy_(pre_trained_dict[layer])
+    for param in expert.parameters():
+        param.requires_grad = True
